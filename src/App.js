ffcb90144a51a98f5dcccfd6b15bd56818c16f84
Neko-Life <nekolife123579@gmail.com>
2023-04-05 08:26:08 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
-import SideBar from './components/SideBar';
-import router from './router/router';
+import Landing from './views/Landing';
 import './App.css';
-import './assets/common.css';
-import { useSelector } from 'react-redux';
-import Console from './components/Console';
 
 function App() {
-  const { showConsole } = useSelector((state) => state);
-  React.useEffect(() => {
-    if (showConsole) {
-      document
-        .querySelector('#console-stdin-form-input')
-        ?.focus({ preventScroll: true });
-    }
-  }, [showConsole]);
-
   return (
     <div className={'App theme-dark'}>
-      <NavBar />
-      <div className="shadow-light main-container">
-        <div className="sidebar-outer-container">
-          <SideBar />
-        </div>
-        <div className="main-main-container">
-          <div className="main-main-container-top">
-            <RouterProvider router={router} />
-          </div>
-          <div className="main-main-container-bottom">
-            <div
-              className={`console-container ${
-                showConsole ? 'console-show' : ''
-              }`}
-            >
-              <Console disabled={!showConsole} />
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
+      <Landing />
     </div>
   );
 }
---
+++ src/views/Landing.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+import SideBar from '../components/SideBar';
+import router from '../router/router';
+import Console from '../components/Console';
+import '../assets/Landing.css';
+import '../assets/common.css';
+
+export default function Landing() {
+  const { showConsole } = useSelector((state) => state);
+  React.useEffect(() => {
+    if (showConsole) {
+      document
+        .querySelector('#console-stdin-form-input')
+        ?.focus({ preventScroll: true });
+    }
+  }, [showConsole]);
+
+  return (
+    <>
+      <NavBar />
+      <div className="shadow-light main-container">
+        <div className="sidebar-outer-container">
+          <SideBar />
+        </div>
+        <div className="main-main-container">
+          <div className="main-main-container-top">
+            <RouterProvider router={router} />
+          </div>
+          <div className="main-main-container-bottom">
+            <div
+              className={`console-container ${
+                showConsole ? 'console-show' : ''
+              }`}
+            >
+              <Console disabled={!showConsole} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
