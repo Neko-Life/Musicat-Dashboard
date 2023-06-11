cc80db6cfffbba3abbe74208f5073f87b610a09e
Neko-Life <nekolife123579@gmail.com>
2023-06-11 20:04:40 +0700

    add login command

--- src/console/commands/login.ts
+++ src/console/commands/login.ts
@@ -1,9 +1,14 @@
 import Command from '@/classes/Command';
+import { getSocket } from '@/socket/instance';
 
 export default class ConsoleLoginCommand extends Command {
   constructor() {
     super('login');
   }
 
-  run() {}
+  run() {
+    const socket = getSocket();
+
+    socket.requestOauthState();
+  }
 }
