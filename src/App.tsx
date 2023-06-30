311295cce3f0c19e4c721249628b2f73534c6e34
Neko-Life <nekolife123579@gmail.com>
2023-06-30 17:35:30 +0700

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Landing from '@/views/Landing';
+import { ThemeProvider } from '@emotion/react';
+import { defaultTheme } from '@/styles/theme';
 import commonStyles from '@/assets/common.module.css';
 import '@/assets/fonts.css';
 import '@/App.css';
 
 function App() {
   return (
-    <div className={`${commonStyles.App} App theme-dark`}>
-      <Landing />
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <div className={`${commonStyles.App} App theme-dark`}>
+        <Landing />
+      </div>
+    </ThemeProvider>
   );
 }
 
