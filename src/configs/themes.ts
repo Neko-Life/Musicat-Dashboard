286e7c0260cb864ec22f7a35870e161affc0e85f
Neko-Life <nekolife123579@gmail.com>
2023-07-01 09:13:23 +0700

    move colors to config

--- src/configs/themes.ts
+++ src/configs/themes.ts
@@ -1,4 +1,4 @@
-export const main = {
+export const main = Object.freeze({
   color: {
     mainBg: '#4a4e69',
     btnHover: 'rgba(0,0,0,0.2)',
@@ -6,5 +6,19 @@ export const main = {
     btnActive: '#4a4e69',
     textWhite: 'white',
     textMuiBtn: '#1976d2',
+    error: 'red',
+    warn: 'yellow',
+    success: 'green',
+    info: 'cyan',
+    serverCardBg: 'white',
+    serverCardRipple: 'blue',
+    buttonRipple: 'white',
+    fade: {
+      100: 'rgba(0,0,0,0.1)',
+      200: 'rgba(0,0,0,0.2)',
+      300: 'rgba(0,0,0,0.3)',
+      400: 'rgba(0,0,0,0.4)',
+      500: 'rgba(0,0,0,0.5)',
+    },
   },
-};
+});
--- src/util/theme.ts
+++ src/util/theme.ts
@@ -1,24 +1,10 @@
 import { CONSOLE_MARGIN_TOP } from '@/configs/constants';
+import { main } from '@/configs/themes';
 
 export const getConsoleMarginTop = () => {
   return `${CONSOLE_MARGIN_TOP}px`;
 };
 
 export const getColors = () => {
-  return Object.freeze({
-    error: 'red',
-    warn: 'yellow',
-    success: 'green',
-    info: 'cyan',
-    serverCardBg: 'white',
-    serverCardRipple: 'blue',
-    buttonRipple: 'white',
-    fade: {
-      100: 'rgba(0,0,0,0.1)',
-      200: 'rgba(0,0,0,0.2)',
-      300: 'rgba(0,0,0,0.3)',
-      400: 'rgba(0,0,0,0.4)',
-      500: 'rgba(0,0,0,0.5)',
-    },
-  });
+  return main.color;
 };
